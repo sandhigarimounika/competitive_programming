819f638777b4f9669af3e8e1aee75e7a30fa17bf
sandhigarimounika <163624944+sandhigarimounika@users.noreply.github.com>
2024-03-21 12:18:04 +0530

    Add files via upload

---
+++ leetcode.2181.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        tmp=ListNode()
+        res=tmp
+        sum=0
+        head=head.next
+        while head:
+            if head.val!=0:  
+                sum=sum+head.val
+            else:
+                tmp2=ListNode()
+                tmp2.val=sum
+                tmp.next=tmp2
+                tmp=tmp.next
+                sum=0
+            head=head.next
+        res=res.next
+        return res
