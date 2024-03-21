2156fd2f873685c8545213160f27109810880e2f
sandhigarimounika <163624944+sandhigarimounika@users.noreply.github.com>
2024-03-21 12:00:00 +0530

    Add files via upload

---
+++ leetcode.2130.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def pairSum(self, head: Optional[ListNode]) -> int:
+        lst=[]
+        while head:
+            lst.append(head.val)
+            head=head.next
+        maxsum=lst[0]+lst[-1]
+        i=0
+        j=len(lst)-1
+        while i<j:
+            if(lst[i]+lst[j]>maxsum):
+                maxsum=lst[i]+lst[j] 
+            i+=1
+            j-=1
+        return maxsum   
